a7d2f8e6f8c754675b3b0184630071414a12ce3c
EmmaRamirez <ramirez.emma.g@gmail.com>
2025-12-24 22:31:00 -0800

    feat: add support for forwarding ref to Select component
    The Select component now forwards its ref using React.forwardRef. This enables parent components to access and control the internal ref of the Select component, which can be useful for focusing, form integration, and accessibility.

--- src/components/ui/Select.tsx
+++ src/components/ui/Select.tsx
@@ -50,7 +50,7 @@ export const Select = React.forwardRef<HTMLSelectElement, SelectProps>(
                 >
                     <button>
                         <div>
-                            <selectedcontent> </selectedcontent>
+                            <selectedcontent></selectedcontent>
                             <svg width="24" height="24" viewBox="0 0 24 24">
                                 <path fill="currentColor" d="m7 10l5 5l5-5z"/>
                             </svg>
